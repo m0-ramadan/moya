f446da9c0909d064282ee4fa9a1c79f09c2e69e1
Mohamed55088 <mohamedramadenzaki@gmail.com>
2026-01-01 23:46:45 +0200

    order_date

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -21,6 +21,7 @@ class OrderController extends Controller
         $validated = $request->validate([
             'service_id'        => 'required|exists:services,id',
             'water_type_id'     => 'nullable',
+            'order_date'        => 'nullable',
             'saved_location_id' => 'required|exists:saved_locations,id',
         ]);
 
