70f44f66a058bba437fa5847f9de2be1153871f5
Mohamed Ramadan <mohamedramadenzaki@gmail.com>
2026-02-07 16:23:17 +0200

    .

--- routes/channels.php
+++ routes/channels.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-Broadcast::channel('user.{userId}', function ($user, $userId) {
-    return (int) $user->id === (int) $userId;
-});
+
+
+// Broadcast::channel('user.{userId}', function ($user, $userId) {
+//     return (int) $user->id === (int) $userId;
+// });
