be4be745adeb4002f7b755d8884de9fd23a39a7c
Mohamed55088 <mohamedramadenzaki@gmail.com>
2026-02-13 22:55:09 +0200

    .

--- app/Events/Order/NewOrderAvailable.php
+++ app/Events/Order/NewOrderAvailable.php
@@ -52,6 +52,7 @@ class NewOrderAvailable implements ShouldBroadcast
                 'user_name' => $this->order->user->name,
                 'user_phone' => $this->order->user->phone, // Ensure phone is populated
                 'service_name' => $this->order->service->name,
+                'avatar' => get_user_image($this->order->user->avatar),
                 'water_type' => $this->order->waterType ? $this->order->waterType->name : 'غير محدد', // Fix null case
                 'location' => [
                     'address' => $savedLocation ? $savedLocation->address : null,
