d98138243b245b69e90186ade670200d37c39de5
Mohamed55088 <mohamedramadenzaki@gmail.com>
2026-05-05 01:36:29 +0300

    .

--- app/Http/Resources/Driver/OrderResource.php
+++ app/Http/Resources/Driver/OrderResource.php
@@ -14,8 +14,8 @@ class OrderResource extends JsonResource
             'customer' => [
                 'id' => $this->user->id,
                 'name' => $this->user->name,
-                'phone' => $this->user->phone,
-                'avatar'       => get_user_image($this->user->avatar),
+                'phone' => $this->user->full_phone,
+                'avatar' => get_user_image($this->user->avatar),
             ],
             'service' => $this->service->name,
             'water_type' => $this->waterType->name ?? null,
